af2dd20f31345509e5d44c8e11303a48ceb34b0a
Tilman Adler <Tilman.Adler@fau.de>
2016-08-19 15:07:43 +0200

    (grails) add dependencies in asset-pipeline

---
+++ grails-app/assets/javascripts/mongobrowser.js
@@ -0,0 +1,23 @@
+//= require mongoBrowser-src/assets/lib/jquery-2.2.3.min.js
+//= require mongoBrowser-src/assets/lib/jquery-ui-1.11.4.custom/jquery-ui.min.js
+
+// <!-- contextMenu -->
+//= require mongoBrowser-src/assets/lib/jquery-contextMenu/jquery.contextMenu.min.js
+//= require mongoBrowser-src/assets/lib/jquery-contextMenu/jquery.ui.position.min.js
+
+// <!-- iclipboard -->
+//= require mongoBrowser-src/assets/lib/clipboardjs/clipboard.min.js
+//= require mongoBrowser-src/assets/lib/jquery-resizable-columns/jquery.resizableColumns.js
+
+
+// <!-- codemirror -->
+//= require mongoBrowser-src/assets/lib/codemirror/lib/codemirror.js
+//= require mongoBrowser-src/assets/lib/codemirror/mode/javascript/javascript.js
+//= require mongoBrowser-src/assets/lib/codemirror/addon/edit/matchbrackets.js
+//= require mongoBrowser-src/assets/lib/codemirror/addon/hint/show-hint.js
+
+//= require mongoBrowser-src/mongobrowser-tabs.js
+//= require mongoBrowser-src/mongobrowser-guiCreation.js
+//= require mongoBrowser-src/mongobrowser.js
+
+//= require mongoBrowser-src/mongo-shell/mongo-shell.js
