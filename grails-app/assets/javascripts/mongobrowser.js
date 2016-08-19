b39667d3ae629e12077b4f23ecd75ef89aa53b29
Tilman Adler <Tilman.Adler@fau.de>
2016-08-19 17:16:35 +0200

    (grails) fix asset pipeline minification

--- grails-app/assets/javascripts/mongobrowser.js
+++ grails-app/assets/javascripts/mongobrowser.js
@@ -1,3 +1,10 @@
+// <!-- codemirror -->
+//= require mongoBrowser-src/assets/lib/codemirror/lib/codemirror.js
+//= require mongoBrowser-src/assets/lib/codemirror/mode/javascript/javascript.js
+//= require mongoBrowser-src/assets/lib/codemirror/addon/edit/matchbrackets.js
+//= require mongoBrowser-src/assets/lib/codemirror/addon/hint/show-hint.js
+
+// <!-- jquery, jquery ui -->
 //= require mongoBrowser-src/assets/lib/jquery-2.2.3.min.js
 //= require mongoBrowser-src/assets/lib/jquery-ui-1.11.4.custom/jquery-ui.min.js
 
@@ -10,14 +17,6 @@
 //= require mongoBrowser-src/assets/lib/jquery-resizable-columns/jquery.resizableColumns.js
 
 
-// <!-- codemirror -->
-//= require mongoBrowser-src/assets/lib/codemirror/lib/codemirror.js
-//= require mongoBrowser-src/assets/lib/codemirror/mode/javascript/javascript.js
-//= require mongoBrowser-src/assets/lib/codemirror/addon/edit/matchbrackets.js
-//= require mongoBrowser-src/assets/lib/codemirror/addon/hint/show-hint.js
-
 //= require mongoBrowser-src/mongobrowser-tabs.js
 //= require mongoBrowser-src/mongobrowser-guiCreation.js
 //= require mongoBrowser-src/mongobrowser.js
-
-//= require mongoBrowser-src/mongo-shell/mongo-shell.js
--- grails-app/views/_mongoBrowser.gsp
+++ grails-app/views/_mongoBrowser.gsp
@@ -1,4 +1,5 @@
 <asset:javascript src="mongobrowser.js" />
+<asset:javascript src="mongoBrowser-src/mongo-shell/mongo-shell.unminified.js" />
 <asset:stylesheet src="mongobrowser.css" />
 <script>
     var m = MongoBrowser($("body")[0],
