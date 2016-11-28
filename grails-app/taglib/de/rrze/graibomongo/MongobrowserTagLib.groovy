1c0e7d875a1eb68e06dea582ce32cb82d975d8c6
Tilman Adler <Tilman.Adler@fau.de>
2016-11-28 18:36:07 +0100

    (taglib) move taglib to graibomongo package

--- grails-app/taglib/de/rrze/graibomongo/MongobrowserTagLib.groovy
+++ grails-app/taglib/de/rrze/graibomongo/MongobrowserTagLib.groovy
@@ -1,11 +1,11 @@
-package de.rrze.mongobrowser
+package de.rrze.graibomongo
 
 class CmeditorTagLib {
 
 	static namespace = "mongoBrowser"
 
 	def block = { attrs, body ->
-		out << render(template: "mongoBrowser", model: [windowMode: "resizable", presets: ([attrs.preset] + attrs.presets)])
+		out << render(template: "mongoBrowser", model: [windowMode: "resizable", presets: ([attrs.preset] + attrs.presets + defaultPresets)])
 	}
 
 	def window = { attrs, body ->
