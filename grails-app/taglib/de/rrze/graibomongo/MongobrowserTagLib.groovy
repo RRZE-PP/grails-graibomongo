11873b4b413298a87d9c3536bec77d171aeefca6
Tilman Adler <Tilman.Adler@fau.de>
2016-11-28 18:38:37 +0100

    (taglib) add basic support for default connections

--- grails-app/taglib/de/rrze/graibomongo/MongobrowserTagLib.groovy
+++ grails-app/taglib/de/rrze/graibomongo/MongobrowserTagLib.groovy
@@ -5,10 +5,44 @@ class CmeditorTagLib {
 	static namespace = "mongoBrowser"
 
 	def block = { attrs, body ->
+		def defaultPresets = []
+		def defaultConnections = grails.util.Holders.grailsApplication?.config?.graibomongo?.defaultConnections
+		if(defaultConnections){
+			for(name in defaultConnections.keySet()){
+				def conn = defaultConnections[name]
+				if(!(conn.host && (!conn.auth || (conn.auth.username && conn.auth.password)))){
+					//not all necesarry data was given
+					continue
+				}
+
+				def preset = conn.port ? new ConnectionPreset(name, conn.host, conn.port) : new ConnectionPreset(name, conn.host)
+				if(conn.auth){
+					preset.auth(conn.auth.username, conn.auth.port)
+				}
+				defaultPresets.put(preset)
+			}
+		}
 		out << render(template: "mongoBrowser", model: [windowMode: "resizable", presets: ([attrs.preset] + attrs.presets + defaultPresets)])
 	}
 
 	def window = { attrs, body ->
-		out << render(template: "mongoBrowser", model: [windowMode: "moveable", presets: ([attrs.preset] + attrs.presets)])
+		def defaultPresets = []
+		def defaultConnections = grails.util.Holders.grailsApplication?.config?.graibomongo?.defaultConnections
+		if(defaultConnections){
+			for(name in defaultConnections.keySet()){
+				def conn = defaultConnections[name]
+				if(!(conn.host && (!conn.auth || (conn.auth.username && conn.auth.password)))){
+					println("Not all necesarry data was given for default preset: " + name)
+					continue
+				}
+
+				def preset = conn.port ? new ConnectionPreset(name, conn.host, conn.port) : new ConnectionPreset(name, conn.host)
+				if(conn.auth){
+					preset.auth(conn.auth.username, conn.auth.password)
+				}
+				defaultPresets.add(preset)
+			}
+		}
+		out << render(template: "mongoBrowser", model: [windowMode: "moveable", presets: ([attrs.preset] + attrs.presets + defaultPresets)])
 	}
 }
