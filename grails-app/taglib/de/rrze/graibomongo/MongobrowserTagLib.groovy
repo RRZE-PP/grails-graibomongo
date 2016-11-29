ac4f7773e257fa0feb6c7e3c2a9cfdebb529a3ca
Tilman Adler <Tilman.Adler@fau.de>
2016-11-29 12:22:13 +0100

    (taglib) extract loading of default presets into method

--- grails-app/taglib/de/rrze/graibomongo/MongobrowserTagLib.groovy
+++ grails-app/taglib/de/rrze/graibomongo/MongobrowserTagLib.groovy
@@ -4,28 +4,7 @@ class CmeditorTagLib {
 
 	static namespace = "mongoBrowser"
 
-	def block = { attrs, body ->
-		def defaultPresets = []
-		def defaultConnections = grails.util.Holders.grailsApplication?.config?.graibomongo?.defaultConnections
-		if(defaultConnections){
-			for(name in defaultConnections.keySet()){
-				def conn = defaultConnections[name]
-				if(!(conn.host && (!conn.auth || (conn.auth.username && conn.auth.password)))){
-					//not all necesarry data was given
-					continue
-				}
-
-				def preset = conn.port ? new ConnectionPreset(name, conn.host, conn.port) : new ConnectionPreset(name, conn.host)
-				if(conn.auth){
-					preset.auth(conn.auth.username, conn.auth.port)
-				}
-				defaultPresets.put(preset)
-			}
-		}
-		out << render(template: "mongoBrowser", model: [windowMode: "resizable", presets: ([attrs.preset] + attrs.presets + defaultPresets)])
-	}
-
-	def window = { attrs, body ->
+	def loadDefaultPresets(){
 		def defaultPresets = []
 		def defaultConnections = grails.util.Holders.grailsApplication?.config?.graibomongo?.defaultConnections
 		if(defaultConnections){
@@ -43,6 +22,14 @@ class CmeditorTagLib {
 				defaultPresets.add(preset)
 			}
 		}
-		out << render(template: "mongoBrowser", model: [windowMode: "moveable", presets: ([attrs.preset] + attrs.presets + defaultPresets)])
+		return defaultPresets
+	}
+
+	def block = { attrs, body ->
+		out << render(template: "mongoBrowser", model: [windowMode: "resizable", presets: ([attrs.preset] + attrs.presets + loadDefaultPresets())])
+	}
+
+	def window = { attrs, body ->
+		out << render(template: "mongoBrowser", model: [windowMode: "moveable", presets: ([attrs.preset] + attrs.presets + loadDefaultPresets())])
 	}
 }
