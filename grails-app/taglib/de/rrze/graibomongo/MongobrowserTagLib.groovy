76341867afd45391cc4c5f49fb07b1452477e4a1
Tilman Adler <Tilman.Adler@fau.de>
2016-11-29 12:56:51 +0100

    (taglib) support all auth parameters in default connections

--- grails-app/taglib/de/rrze/graibomongo/MongobrowserTagLib.groovy
+++ grails-app/taglib/de/rrze/graibomongo/MongobrowserTagLib.groovy
@@ -17,8 +17,24 @@ class CmeditorTagLib {
 
 				def preset = conn.port ? new ConnectionPreset(name, conn.host, conn.port) : new ConnectionPreset(name, conn.host)
 				if(conn.auth){
-					preset.auth(conn.auth.username, conn.auth.password)
+					def auth = conn.auth
+					if(auth.database &&  auth.method && auth.method in ["scram-sha-1", "mongodb-cr"]){
+						preset.auth(auth.username,
+						            auth.password,
+						            auth.database,
+						            "scram-sha-1".equals(auth.method) ? ConnectionPreset.Method.SCRAM_SHA_1 : ConnectionPreset.Method.MONGODB_CR)
+					}else if(auth.database){
+						preset.auth(auth.username, auth.password, auth.database)
+					}else if(auth.method && auth.method in ["scram-sha-1", "mongodb-cr"]){
+						preset.auth(auth.username,
+						            auth.password,
+						            "admin",
+						            "scram-sha-1".equals(auth.method) ? ConnectionPreset.Method.SCRAM_SHA_1 : ConnectionPreset.Method.MONGODB_CR)
+					}else{
+						preset.auth(auth.username, auth.password)
+					}
 				}
+
 				defaultPresets.add(preset)
 			}
 		}
